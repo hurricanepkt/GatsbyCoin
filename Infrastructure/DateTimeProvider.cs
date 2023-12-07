4cdfd564ebd63f563a3a478aba732d7e15864aff
Mark Greenway <mark.greenway@LIGMarine.com>
2023-12-07 18:04:41 -0500

    Try catch on startup

--- Infrastructure/DateTimeProvider.cs
+++ Infrastructure/DateTimeProvider.cs
@@ -8,15 +8,14 @@ public class DateTimeProvider : IDateTimeProvider {
   private DateTime _now;
   public DateTimeProvider()
   {
-    _now = DateTime.Now;
+    _now = DateTime.Parse("2023-12-01");
   }
   public DateTime Now 
   {
     get
     {
-      //_now = _now.AddMilliseconds(157);
-      //return _now;
-      return DateTime.Now;
+      _now = _now.AddMilliseconds(157);
+      return _now;
     }
   }
   public string IsoWeek => System.Globalization.ISOWeek.GetWeekOfYear(Now).ToString("D2");
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
     options.SerializerOptions.PropertyNameCaseInsensitive = false;
     options.SerializerOptions.PropertyNamingPolicy = null;
     options.SerializerOptions.WriteIndented = true;
-    
+
 });
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<HandleBlockService>();
@@ -18,12 +18,21 @@ builder.Services.AddSingleton<Chain>();
 var app = builder.Build();
 
 var chain = app.Services.GetRequiredService<Chain>();
-string [] fileEntries = Directory.GetFiles(Path.Join(Environment.CurrentDirectory, "Files"));
-foreach(string fileName in fileEntries) {
-    chain.AddBlock(File.ReadAllText(fileName));
+try
+{
+    string[] fileEntries = Directory.GetFiles(Path.Join(Environment.CurrentDirectory, "Files"));
+    foreach (string fileName in fileEntries)
+    {
+        chain.AddBlock(File.ReadAllText(fileName));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
 }
 //chain.AddBlock("First block");
 
 app.MapGet("/api/chain", () => chain.TheChain);
+app.UseStaticFiles();
 app.MapRazorPages();
 app.Run();
